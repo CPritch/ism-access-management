74d632028bf926ee88c20efe00f8a295917ae46e
CPritch <CPritch@users.noreply.github.com>
2024-08-04 17:49:37 +0100

    feat: remove silly interface classes

--- AccessManager/Data/IDoorRepository.cs
+++ AccessManager/Data/IDoorRepository.cs
@@ -4,8 +4,5 @@
     {
         List<Door> GetAllDoors();
         Door GetDoorById(int id);
-        void SetOpen(int id, bool open);
-        void SetLocked(int id, bool locked);
-        void SetAlarmed(int id, bool alarmed);
     }
 }
