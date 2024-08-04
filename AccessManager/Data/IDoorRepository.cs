c7ba6f3cae7ee29f5a02f317fdbe6999fa24e57f
CPritch <CPritch@users.noreply.github.com>
2024-08-04 17:15:23 +0100

    feat: door repo interface

---
+++ AccessManager/Data/IDoorRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace AccessManager.Data
+{
+    public interface IDoorRepository
+    {
+        List<Door> GetAllDoors();
+        Door GetDoorById(int id);
+        void SetOpen(int id, bool open);
+        void SetLocked(int id, bool locked);
+        void SetAlarmed(int id, bool alarmed);
+    }
+}
