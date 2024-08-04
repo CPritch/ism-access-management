66ad7ea6daf630ca9dda030333e7378770417ae5
CPritch <CPritch@users.noreply.github.com>
2024-08-04 17:40:47 +0100

    feat: starts of local door repo

--- AccessManager/Data/DoorRepository.cs
+++ AccessManager/Data/DoorRepository.cs
@@ -1,6 +1,17 @@
 ﻿namespace AccessManager.Data
 {
-    public class DoorRepository
+    public class DoorRepository : IDoorRepository
     {
+        private List<Door> _doors = new List<Door>
+        {
+            new Door(id: 1, name: "Front Entrance"),
+            new Door(id: 2, name: "Side Entrance" ),
+            new Door(id: 3, name: "Rear Entrance" ),
+            new Door(id: 4, name: "Secure Locker" ),
+        };
+
+        public List<Door> GetAllDoors() { return _doors; }
+
+        public Door GetDoorById(int id) { return _doors[id]; }
     }
 }
