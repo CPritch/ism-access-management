c5fd278ce4266bb7e75d7d6da4b3d8bd68f7e6d6
CPritch <CPritch@users.noreply.github.com>
2024-08-04 17:05:39 +0100

    feat: finish simple door model

--- AccessManager/Data/Door.cs
+++ AccessManager/Data/Door.cs
@@ -1,6 +1,25 @@
-﻿namespace AccessManager.Data
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AccessManager.Data
 {
     public class Door
     {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "Door name cannot exceed 50 characters.")]
+        public string Name { get; set; }
+        public bool IsOpen { get; set; }
+        public bool IsLocked { get; set; }
+        public bool IsAlarmed { get; set; }
+
+        public Door(int id, string name)
+        {
+            Id = id;
+            Name = name;
+            IsOpen = false;
+            IsLocked = false;
+            IsAlarmed = false;
+        }
     }
 }
